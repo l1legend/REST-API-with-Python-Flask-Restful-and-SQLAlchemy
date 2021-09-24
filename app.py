5b0ca3d853e7f5d9c13a3cfddedba73869a182c4
minTwin <michaelmin2020@gmail.com>
2021-09-24 17:02:54 +0200

    test change

--- app.py
+++ app.py
@@ -11,8 +11,7 @@ from resources.store import Store, StoreList
 
 app = Flask(__name__)
 
-app.config['DEBUG'] = True
-
+app.config['DEBUG'] = True #testing
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
